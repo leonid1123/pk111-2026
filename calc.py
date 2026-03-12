7448f0e4a6b978236dbe21f93600bdf8da9f25e3
Leo <leonid112@yandex.ru>
2026-03-12 12:18:00 +0300

    начало

--- calc.py
+++ calc.py
@@ -1,27 +1,59 @@
-from tkinter import *
-from tkinter import ttk
+import sqlite3
+import random
 
 
-class App(Tk):
+class App:
     def __init__(self):
-        super().__init__()
-        self.pic = PhotoImage(file='flag.png')
-        for _row in range(0, 9):
-            for _col in range(0, 9):
-                tmp_btn = Button(self, image=self.pic)
-                tmp_btn.bind('<Button-1>', self.btn_remove)
-                tmp_btn.bind('<Button-3>', self.btn_mark)
-                tmp_btn.grid(row=_row, column=_col)
+        self.conn = sqlite3.connect('dice.db')
+        self.cur = self.conn.cursor()
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS Results(
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Пункт_игры TEXT,
+        Описание TEXT,
+        Сумма INTEGER)    
+        ''')
+        self.conn.commit()
 
-    def btn_remove(self, event):
-        print('левая кнопка')
-        print(event)
+        self.cur.execute('SELECT * FROM Results')
+        ans = self.cur.fetchall()
+        if len(ans) < 1:
+            sql = 'INSERT INTO Results(Пункт_игры, Описание) VALUES(?,?)'
+            info = (
+                ('Школа 1', 'Три кости с гранью 1'),
+                ('Школа 2', 'Три кости с гранью 2'),
+                ('Школа 3', 'Три кости с гранью 3'),
+                ('Школа 4', 'Три кости с гранью 4'),
+                ('Школа 5', 'Три кости с гранью 5'),
+                ('Школа 6', 'Три кости с гранью 6'),
+                ('Пара', 'Две кости с равными гранями'),
+                ('Треугольник', 'Три кости с равными гранями'),
+                ('Каре', 'Четыре кости с равными гранями'),
+                ('Покер', 'Пять костей с равными гранями'),
+                ('Две пары', 'Две пары равных граней'),
+                ('Фул', 'Две кости с равными гранями и три кости с равными гранями'),
+                ('Малая дорога', 'Комбинация значений граней 1-2-3-4-5'),
+                ('Большая дорога', 'Комбинация значений граней 2-3-4-5-6'),
+                ('Сумма', 'Любая комбинация значений граней'),
+                    )
+            self.cur.executemany(sql, info)
+            self.conn.commit()
 
-    def btn_mark(self, event):
-        print('правая кнопка')
-        print(event)
+        self.cur.execute('SELECT * FROM Results')
+        ans = self.cur.fetchall()
+        for item in ans:
+            print(item)
+
+        name1 = input('Как зовут первого игрока? ')
+        name2 = input('Как зовут второго игрока? ')
+
+        d1 = random.randint(1,6)
+        d2 = random.randint(1, 6)
+        d3 = random.randint(1, 6)
+        d4 = random.randint(1, 6)
+        d5 = random.randint(1, 6)
+        print(f'Игрок {name1} выбросил: {d1} {d2} {d3} {d4} {d5}')
 
 
 
 app = App()
-app.mainloop()
+app.conn.close()
